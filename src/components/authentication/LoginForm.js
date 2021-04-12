60694282f053bbb8f4e9c3d760eec04fb9990f2d
Hamer Patarroyo <hamer.patarroyo@gmail.com>
2021-04-12 10:32:03 -0500

    adding sass library, also creating login form component and cleaning app.js

---
+++ src/components/authentication/LoginForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './loginform.scss'
+
+export default function LoginForm() {
+    return (
+        <div className="formContainer">
+            <h1 className="title">Sign In</h1>
+        </div>
+    )
+}
